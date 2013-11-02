56814f2cb6d183f829a4e35090a611aca645f347
Michael Nisi <michael.nisi@gmail.com>
2013-11-02 18:48:43 +0100

    Write http server

--- example/server.js
+++ example/server.js
@@ -1,10 +1,11 @@
 
 var http = require('http')
-  , manger = require('../')
   , levelup = require('levelup')
   , path  = require('path')
   , assert = require('assert')
   , fs = require('fs')
+  , Store = require('../lib/manger').Store
+  , es = require('event-stream')
 
 var dir = '/tmp/manger-' + Math.floor(Math.random() * (1<<24))
   , loc = path.join(dir, 'test.db')
@@ -15,13 +16,11 @@ fs.mkdirSync(dir, 0700)
 levelup(loc, opts, function (er, db) {
   assert(!er && db)
   http.createServer(function (req, res) {
-    if (req.method === 'POST') {
-      var opts = {}
-      opts.db = db
-      req.pipe(manger(opts)).pipe(res)
-    } else {
-      res.writeHead(200)
-      res.end('not yet\n')
-    }
+      var tuples = [
+        ['troubled.pro/rss.xml', 2013, 10]
+      , ['feeds.muleradio.net/allmodcons', 2013]
+      ]
+      es.readArray(tuples)
+        .pipe(new Store(db)).pipe(res)
   }).listen(8765)
 })
