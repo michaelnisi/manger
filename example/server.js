f319571c7aaba20ac67631353ed188ca7528339d
Michael Nisi <michael.nisi@gmail.com>
2013-11-04 18:16:37 +0100

    Serve combined feeds

--- example/server.js
+++ example/server.js
@@ -17,19 +17,48 @@ fs.mkdirSync(dir, 0700)
 levelup(loc, opts, function (er, db) {
   assert(!er && db)
   http.createServer(function (req, res) {
-    var tokens = req.url.split('/')
-    tokens.shift()
-    var token = tokens.shift()
-    var uri = tokens.join('/')
-    var tuple = tupleFromUrl(uri)
-    if (token != 'feeds' || !tuple) {
-      res.writeHead(200)
-      res.end('not found\n')
-      return
-    }
+    var body = ''
+    req.setEncoding('utf8')
+    req.on('data', function (chunk) {
+      body += chunk
+    })
+    req.on('end', function () {
+      respond(req, res, db, body)
+    })
+  }).listen(8765)
+})
+
+function end (res) {
+  res.writeHead(200)
+  res.end('not found\n')
+}
+
+function respond (req, res, db, body) {
+  var tokens = req.url.split('/')
+  tokens.shift()
+  var token = tokens.shift()
+  var uri = tokens.join('/')
+  if (token != 'feeds') {
+    end(res)
+    return
+  }
+  var tuple = tupleFromUrl(uri)
+  if (tuple) {
     var store = new Store(db)
     store.pipe(res)
     store.write(tuple)
     store.end()
-  }).listen(8765)
-})
+    return
+  }
+  try {
+    var data = JSON.parse(body)
+  } catch (er) {
+    console.error(er)
+    end(res)
+    return
+  }
+  // TODO: combined feeds are not working yet
+  es.readArray(data.feeds)
+    .pipe(new Store(db))
+    .pipe(res)
+}
