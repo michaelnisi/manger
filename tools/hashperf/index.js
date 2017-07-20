3f8182850e762208fd178b326e13ffb61d14415f
Michael Nisi <michael.nisi@gmail.com>
2017-07-20 16:06:37 +0200

    Add tools

---
+++ tools/hashperf/index.js
@@ -0,0 +1,32 @@
+const crypto = require('crypto')
+const xxhash = require('xxhash')
+const fs = require('fs')
+const benchmark = require('benchmark');
+const benchmarks = require('beautify-benchmark');
+
+const words = fs.readFileSync('/usr/share/dict/words',
+  { encoding: 'utf8' }
+).split('\n')
+words.rnd = function () {
+  return this[Math.floor(Math.random() * this.length)]
+}
+
+function testSHA() {
+  crypto.createHash('sha1').update(words.rnd()).digest('hex')
+}
+
+function testXXHash() {
+  xxhash.hash(Buffer.from(words.rnd()), 0xCAFEBABE)
+}
+
+const suite = new benchmark.Suite();
+
+suite.on('start', (e) => { process.stdout.write('Working...\n\n') })
+suite.on('cycle', (e) => { benchmarks.add(e.target) })
+suite.on('complete', () => { benchmarks.log() })
+
+suite.add('md5', { minSamples: 100, fn: testSHA })
+suite.add('xxhash', { minSamples: 100, fn: testXXHash })
+
+suite.run({ async: false })
+
