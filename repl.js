8ec2f8cb27e48133f7ca91fdb5f1223d67d1a6a5
Michael Nisi <michael.nisi@gmail.com>
2019-03-08 07:11:00 +0100

    Write cleaner REPL

--- repl.js
+++ repl.js
@@ -6,85 +6,57 @@ const fs = require('fs')
 const lino = require('lino')
 const manger = require('./')
 const repl = require('repl')
-const util = require('util')
-
-const ctx = repl.start({
-  prompt: 'manger> ',
-  ignoreUndefined: true,
-  input: process.stdin,
-  output: process.stdout
-}).context
+const { inspect } = require('util')
 
 const name = process.argv[2] || '/tmp/manger-repl'
-const svc = manger(name, {
-  objectMode: true
+const cache = manger(name, { objectMode: true })
+
+const server = repl.start({
+  ignoreUndefined: true,
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'manger> ',
+  useColors: true
 })
 
-const entries = svc.entries()
-const feeds = svc.feeds()
-const list = () => {
-  const s = svc.list()
-  s.on('error', console.error)
-  return s
-}
-const update = function () {
-  const s = svc.update()
-  s.on('error', (er) => {
-    console.error(er.stack)
+function read (s, prop) {
+  const { log } = console
+
+  s.once('error', er => {
+    log(er)
+  }).on('readable', () => {
+    let obj
+    while ((obj = s.read()) !== null) {
+      let item = prop ? obj[prop] : obj
+
+      log(inspect(item, { colors: true }))
+    }
+  }).on('end', () => {
+    log('ok')
+    server.displayPrompt()
   })
-  return s
-}
-const ranks = function (limit) {
-  const s = svc.ranks(limit)
-  s.on('error', console.error)
-  return s
-}
-const resetRanks = function () {
-  svc.resetRanks((er) => {
-    if (er) console.error(er)
-  })
+
 }
 
-;[entries, feeds].map((s) => {
-  s.on('error', console.error)
-})
-
-function read (stream, prop) {
-  let obj
-  while ((obj = stream.read()) !== null) {
-    console.log(util.inspect(
-      prop ? obj[prop] : obj, { colors: true }))
-  }
-}
-
+// Fills the cache with some feeds, many feeds…
 function fill () {
   const lines = lino()
   let ok = true
+
   function _read () {
     let chunk
     while (ok && (chunk = lines.read()) !== null) {
       ok = feeds.write(chunk)
     }
-    if (!ok) {
-      feeds.once('drain', _read)
-    }
+    if (!ok) feeds.once('drain', _read)
   }
+
   lines.on('readable', _read)
-  fs.createReadStream('./test/data/feeds')
-    .pipe(lines)
+  fs.createReadStream('./test/data/feeds').pipe(lines)
 }
 
-function flushCounter () {
-  svc.flushCounter()
-}
+const { context } = server
 
-ctx.entries = entries
-ctx.feeds = feeds
-ctx.fill = fill
-ctx.flushCounter = flushCounter
-ctx.list = list
-ctx.ranks = ranks
-ctx.read = read
-ctx.resetRanks = resetRanks
-ctx.svc = svc
-ctx.update = update
+context.cache = cache
+context.fill = fill
+context.read = read
